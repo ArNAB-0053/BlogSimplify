dd0e57c2e2378ff9b31a2e33cabe891261d55a7f
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2024-02-08 19:29:36 +0530

    feat: Adding a loader

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/Components/Header";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,8 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className} flex flex-col overflow-x-hidden overflow-y-auto`}>
-          <Header/>
+          <NextTopLoader color="#402aff" />
+          <Header />
           {children}
         </body>
       </html>
